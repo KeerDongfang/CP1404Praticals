03064c961180c683d3e9ec6d4c37794dcbb7de29
KeerDongfang <TR7Rghu@guJsk7d>
2023-07-17 14:28:50 +0800

    first try on extract method

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,7 +1,14 @@
-password = input("Please enter the password:")
+def main():
+    password = input("Please enter the password:")
+    check_password_validity(password)
 
-while len(password) <= 0:
-    print("Invalid password.")
-    password = input("Please enter the password.")
-encrypt_password = '*' * len(password)
-print(encrypt_password)
+
+def check_password_validity(password):
+    while len(password) <= 0:
+        print("Invalid password.")
+        password = input("Please enter the password.")
+    encrypt_password = '*' * len(password)
+    print(encrypt_password)
+
+
+main()
