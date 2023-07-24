45dd11bf26467b979765c44af4b67e2234cb8b9b
KeerDongfang <TR7Rghu@guJsk7d>
2023-07-24 16:04:50 +0800

    first version

---
+++ prac_03/randoms.py
@@ -0,0 +1,13 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+"""The smallest number is 5 and the biggest is 19."""
+
+print(random.randrange(3, 10, 2))  # line 2
+"""The smallest number is 3 and the biggest is 9.In this line, 4 can't ve produced."""
+
+print(random.uniform(2.5, 5.5))  # line 3
+"""The smallest number is 2.5 and the biggest is 5.5."""
+
+print(random.randint(1, 101))
+
